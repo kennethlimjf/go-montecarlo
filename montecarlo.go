2dcc50976cad8d1fc802b23e182c5cd7427b75f6
Kenneth Lim <kennethlimjf@gmail.com>
2015-01-25 02:30:22 +0800

    Refactored variable names

--- montecarlo.go
+++ montecarlo.go
@@ -10,16 +10,15 @@ func main() {
 	annual_inv := 50000.0
 	years := 10
 
-	simulations := 1000000
+	portfolio_returns := 0.05
+	portfolio_sd := 0.12
 
-	var base_ip [2]float64
-	base_ip[0] = 0.05 // Average annual returns
-	base_ip[1] = 0.12 // Standard deviation
+	simulations := 1000000
 
 	chns := make([]chan float64, simulations)
 	for i := 0; i < simulations; i++ {
 		chns[i] = make(chan float64)
-		go calc_total_years_amt(begin_amt, base_ip, annual_inv, years, chns[i])
+		go calc_total_years_amt(begin_amt, portfolio_returns, portfolio_sd, annual_inv, years, chns[i])
 	}
 
 	projections := make([]float64, simulations)
@@ -33,13 +32,13 @@ func main() {
 	for _, v := range projections {
 		sum += v
 	}
-	fmt.Printf("This Monte Carlo Simulation says you can save $%10.2f in %d years if you start with $%.2f and invest $%.2f annually.\n", sum/float64(simulations), years, begin_amt, annual_inv)
+	fmt.Printf("You can save $%10.2f in %d years if you start with $%.2f and invest $%.2f annually.\n", sum/float64(simulations), years, begin_amt, annual_inv)
 }
 
-func calc_total_years_amt(begin_amt float64, base_ip [2]float64, annual_inv float64, years int, c chan float64) float64 {
+func calc_total_years_amt(begin_amt float64, p_ret float64, p_sd float64, annual_inv float64, years int, c chan float64) float64 {
 	sum := begin_amt
 	for i := 0; i < years; i++ {
-		ret := generate_rand_ret(base_ip[0], base_ip[1])
+		ret := generate_rand_ret(p_ret, p_sd)
 		sum += calc_annual_amt(begin_amt, ret, annual_inv)
 	}
 	c <- sum
